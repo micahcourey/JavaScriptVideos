c8038e479430f368d1e603db3bb6f97544b774f4
micahcourey <micahjcourey@gmail.com>
2016-03-03 08:56:27 -0800

    Add initial project files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Create a new component. This component should product some HTML
+const App = function() {
+  return <div>Hi!</div>;
+}
+
+
+// Take this components generated HTML and put it on the page (in the DOM)
