dd563d40dd4d18414b46eb9aebd28060db84c39a
micahcourey <micahjcourey@gmail.com>
2016-03-06 13:35:11 -0800

    Refactor App component as a class component to allow the use of youtube-api-search within the App component.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyAOOQQAVUa5bIcsWq59ayH9gZDB5XDZrRQ';
 
-// Create a new component. This component should product some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'wakesurf'}, (videos ) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
-// Take this components generated HTML and put it on the page (in the DOM)
 ReactDom.render(<App />, document.querySelector('.container'))
